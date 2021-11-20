8b30c750b64d262f88a57ffcd8564fa857880389
unknown <razvandra33@gmail.com>
2021-11-20 11:46:06 +0100

    first working version

--- backend/tuhh_campus_shop/models.py
+++ backend/tuhh_campus_shop/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+
 ##### Andrei
 class ProductQuerySet(models.QuerySet):
 
@@ -27,17 +30,17 @@ class ProductQuerySet(models.QuerySet):
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
+
 
 
 class Product(models.Model):
-    product_id = models.AutoField(db_column='product_ID', unique=True)  # Field name made lowercase.
+    product_id = models.AutoField(db_column='product_ID', primary_key=True)  # Field name made lowercase.   ######################################## changed from uniwque to primary_key
     product_stock = models.PositiveIntegerField(db_column='product_Stock', blank=True, null=True)  # Field name made lowercase.
     product_name = models.TextField(db_column='product_Name', blank=True, null=True)  # Field name made lowercase.
     product_description = models.TextField(db_column='product_Description', blank=True, null=True)  # Field name made lowercase.
     product_specifications = models.TextField(db_column='product_Specifications', blank=True, null=True)  # Field name made lowercase.
     picture_path = models.TextField(db_column='picture_Path', blank=True, null=True)  # Field name made lowercase.
-    product_price = models.PositiveIntegerField(db_column='product_Price', blank=True, null=True)  # Field name made lowercase.
+    product_price = models.DecimalField(db_column='product_Price', max_digits=30, decimal_places=2, blank=True, null=True)  # Field name made lowercase.
     product_reviews = models.PositiveIntegerField(db_column='product_Reviews', blank=True, null=True)  # Field name made lowercase.
 
     
@@ -47,6 +50,7 @@ class Product(models.Model):
     class Meta:
         managed = False
         db_table = 'PRODUCT'
+        app_label = 'product_db'
     
 
 
