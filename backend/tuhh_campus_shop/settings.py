351a18d026e6bcd612eaab08b2b8809b7665a18f
unknown <razvandra33@gmail.com>
2021-11-21 19:55:39 +0100

    added allowed hosts

--- backend/tuhh_campus_shop/settings.py
+++ backend/tuhh_campus_shop/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-1#8g&#cw*y=fk%uv-y+x1)q#us8!yun#k^hlxtyr)qb$ffflei
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'tuhh_campus_shop.urls'
